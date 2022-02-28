22c7bd1203bb2783212d92162d8ecfc6523157be
yue zhi <yy.yuezhi139@gmail.com>
2022-02-28 15:23:36 +0800

    new update

--- company2/mgr/url.py
+++ company2/mgr/url.py
@@ -1,12 +1,13 @@
 from django.urls import path
 # from mgr.customer import dispatcher
-from mgr import customer
-from mgr import sign_in_out
+from . import customer, sign_in_out, medicine ,order
 
 # 子路由表
 urlpatterns = [
 
     path('customers', customer.dispatcher),
+    path('medicines', medicine.dispatcher),
+    path('orders', order.dispatcher),
     path('signin', sign_in_out.signin),
     path('signout', sign_in_out.signout),
 
